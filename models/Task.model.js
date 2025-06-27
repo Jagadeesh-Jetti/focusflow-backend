5550197d1327278e0f21ea3370bcc9af75294484
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-06-27 23:47:43 +0530

    feat: created task model

---
+++ models/Task.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  milestone: { type: mongoose.Schema.Types.ObjectId, ref: 'Milestone' },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  dueDate: { type: Date },
+  priority: {
+    type: String,
+    enum: ['low', 'medium', 'high'],
+    default: 'medium',
+  },
+  isCompleted: { type: Boolean, default: false },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
