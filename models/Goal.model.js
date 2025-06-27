315e0a355295272dde65eff8812cbb13ac6f1e4e
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-06-27 23:47:27 +0530

    feat: created goal model

---
+++ models/Goal.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const goalSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  milestones: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Milestone' }],
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Goal = mongoose.model('Goal', goalSchema);
+module.exports = Goal;
