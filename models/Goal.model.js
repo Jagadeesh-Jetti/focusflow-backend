73524d828e3a2ab347a406c7399ab5953dc620ad
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-03 23:42:57 +0530

    fix: updated goal schema

--- models/Goal.model.js
+++ models/Goal.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const goalSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   category: { type: String, required: true },
