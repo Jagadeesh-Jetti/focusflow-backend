b0b928b5fc6ac575f839ca8f205298cc9cccfb07
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-07 23:25:12 +0530

    refactor: updated cors

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello world');
