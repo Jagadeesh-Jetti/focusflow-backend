c65ef0bdcb4b8c4a7e53748733101be53a5941f7
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-07-24 21:23:57 +0530

    feat: added user model

--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,17 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  profilePic: { type: String, default: '' },
+  bannerPic: { type: String, default: '' },
   password: { type: String, required: true },
+
+  bio: { type: String, default: '' },
+  location: { type: String, default: '' },
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Types.ObjectId, ref: 'User' }],
+  likes: { type: Number, dafault: 0 },
+
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
   goals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Goal' }],
   createdAt: { type: Date, default: Date.now },
 });
