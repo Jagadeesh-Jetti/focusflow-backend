68bc7a526d897b499a099dd31c542b575ff60e34
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2026-02-03 17:00:57 +0530

    refactor: add frontend url for cors

--- index.js
+++ index.js
@@ -18,9 +18,23 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://focus-flow-theta-henna.vercel.app',
+];
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
